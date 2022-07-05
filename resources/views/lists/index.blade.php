53c166b4791493f1e62b83dbb2fc10bd1a75ccfe
Asuka <72805418+flyingbirdasuka@users.noreply.github.com>
2022-07-05 21:35:07 +0200

    Update index.blade.php

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -8,14 +8,12 @@
                 <div class="card-header">Latest music lists :)
                     <a href="/lists/create"class="float-right">create a new list!</a>
                 </div>
-
-                
                    @if(count($lists) > 0) 
                        @foreach($lists as $list)
                        <div class="card-body">
                        <h3>{{ $list->title}}</h3>
                          <img src="/storage/album-covers/{{ $list->cover_image}}" width="350px">
-                       <p> {{ $list->description}}</p> 
+                       <p>{{ $list->description}}</p> 
                        <div class="btn-group">
                           <a type="button" class="btn btn-info btn-outline-secondary" href="/lists/{{$list->id}}">View</a>
                        </div> 
